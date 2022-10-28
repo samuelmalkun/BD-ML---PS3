bd4b09261ee3b805c59c8371585e705b77179043
Diego <df.osorio11@uniandes.edu.co>
2022-10-27 19:43:50 -0500

    up

--- scripts/diego/dataclean.R
+++ scripts/diego/dataclean.R
@@ -38,6 +38,12 @@ available_tags("amenity")
 ## obtener la caja de coordenada que contiene el polígono de Cali
 opq(bbox = getbb("Cali Colombia"))
 
+## obtener la caja de coordenada que contiene el polígono de Bogotá, DC
+opq(bbox = getbb("Bogotá Colombia"))
+
+## obtener la caja de coordenada que contiene el polígono de Medellín
+opq(bbox = getbb("Medellín Colombia"))
+
 
 
 # crear lista de amenities
@@ -53,8 +59,10 @@ lista_amenities <- c("bank","restaurant", "bus_station", "police", "bank", "cafe
                      "theatre", "veterinary", "school", "university", "arts_centre")
 
 
-#El loop crea un objeto osm para cada amenitie y extrae los simple features
-for (amenitie in lista_amenities[1:2]) {
+
+#Crear objetos OSM para cada amenitie y extraer los Simple Features tipo Poligonos
+#El loop crea un objeto OSM para cada amenitie y extrae los Simple Features
+for (amenitie in lista_amenities) {
   #Crear objetos osm
   #assign(paste("osm", amenitie, sep = "_"), opq(bbox = getbb("Cali Colombia")) %>%
   #        add_osm_feature(key="amenity" , value= amenitie))
@@ -75,7 +83,6 @@ for (amenitie in lista_amenities[1:2]) {
   assign(paste("osm", amenitie,"Medellin" ,"sf", sep = "_"), opq(bbox = getbb("Medellín Colombia")) %>%
            add_osm_feature(key="amenity" , value= amenitie) %>% osmdata_sf() %>% 
            .$osm_polygons)
-  
   
 }
 
@@ -109,11 +116,6 @@ bus_station
 leaflet() %>% addTiles() %>% addCircleMarkers(data=bus_station , col="red")
 
 
-
-
-
-
-
 ## **[5.] Operaciones geometricas**
 
 ### **5.1 Importar conjuntos de datos**
@@ -123,7 +125,7 @@ leaflet() %>% addTiles() %>% addCircleMarkers(data=bus_station , col="red")
 #class(train_house)
 #skim(train_house)
 
-## converir DataFrame de train-set a sf
+## convetir DataFrame de train-set a sf
 train_house <- st_as_sf(x = train, ## datos
                    coords=c("lon","lat"), ## coordenadas
                    crs=4326) ## CRS
@@ -143,27 +145,33 @@ vignette("sf4")
 ### **5.3 Afine transformations**
 st_crs(train_house) == st_crs(parques) 
 
+
+
 ### **5.4 Filtrar datos**
-
 ## usando los valores de una variable
-
-# grafico casas Medellin
-houses_medellin <- train_house %>% subset(city= "Medellin")
-
-leaflet() %>% addTiles() %>% addCircles(data=houses_medellin)
 
 
 
 # grafico casas Bogota
 
 # opcion 1: grafico general de casas de Bogota con filtro de variable city
+# casas de Bogota
 houses_bogota <- train_house %>% subset(city= "Bogotá D.C")
 
 leaflet() %>% addTiles() %>% addCircles(data=houses_bogota)
 
+# casas de Medellín
+houses_medellin <- train_house %>% subset(city= "Medellín")
+
+leaflet() %>% addTiles() %>% addCircles(data=houses_medellin)
+
+
+
 
 
 ### Usar la geometría de la ciudad
+
+## crear sf_polygon de las ciudades
 # crear sf_polygon de la ciudad Bogota
 bogota_city <- getbb(place_name = "Bogota", 
                    featuretype = "boundary:administrative", 
@@ -173,10 +181,21 @@ bogota_city <- getbb(place_name = "Bogota",
 leaflet() %>% addTiles() %>% addPolygons(data=bogota_city)
 
 
+# crear sf_polygon de la ciudad Medellin
+medellin_city <- getbb(place_name = "Medellín", 
+                     featuretype = "boundary:administrative", 
+                     format_out = "sf_polygon") %>% .$multipolygon
+
+# graficar sf_polygon de la ciudad de Bogota sf_polygon
+leaflet() %>% addTiles() %>% addPolygons(data=medellin_city)
+
+
+
 ## crear interseccion entre x base de casas sf, y sf_polygon   
 house_chapi <- st_intersection(x = houses_bogota , y = bogota_city)
 # graficar sf_polygon de la interseccion entre ambos sf
 leaflet() %>% addTiles() %>% addPolygons(data=bogota_city,col="red") %>% addCircles(data=house_chapi)
+
 
 
 
@@ -191,6 +210,9 @@ leaflet() %>% addTiles() %>% addPolygons(data=bogota_city,col="red") %>% addCirc
 ## Calcular Distancia a muchos polygonos
 
 ## Distancia a amenities Bogota
+
+
+# arts_centre_bog
 matrix_dist_arts_centre_bog <- st_distance(x=house_chapi , y=osm_arts_centre_Bogota_sf)
 
 #matrix_dist_parque[1:5,1:5]
